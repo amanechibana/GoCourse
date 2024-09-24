04e15a63a3d60075cc61607ba896cd19d8f3ed2e
amanechibana <118863897+amanechibana@users.noreply.github.com>
2024-09-24 16:30:25 -0400

    updated struct

--- Structs/structs.go
+++ Structs/structs.go
@@ -5,29 +5,41 @@ import (
 	"time"
 )
 
-type user struct{
+type user struct {
 	firstName string
-	lastName string
+	lastName  string
 	birthdate string
 	createdAt time.Time
 }
 
-func (u user) outputValue(){
-	fmt.Println(u.firstName,u.lastName,u.birthdate,u.createdAt)
+func (u user) outputValue() {
+	fmt.Println(u.firstName, u.lastName, u.birthdate, u.createdAt)
 }
 
-func (u *user) clearUserName(){
+func (u *user) clearUserName() {
 	u.firstName = ""
 	u.lastName = ""
 }
 
-func main() {
-	var appUser user = user{
-		getUserData("Please enter your first name: "),
-		getUserData("Please enter your last name: "),
-		getUserData("Please enter your birthdate (MM/DD/YYYY): "),
+func newUser(firstName, lastName, birthdate string) *user {
+	if firstName == "" || lastName == "" || birthdate == "" {
+
+	}
+
+	return &user{
+		firstName,
+		lastName,
+		birthdate,
 		time.Now(),
 	}
+}
+
+func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirth := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var appUser *user = newUser(userFirstName, userLastName, userBirth)
 
 	appUser.outputValue()
 	appUser.clearUserName()
