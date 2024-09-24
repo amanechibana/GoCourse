5f92dc81a7414a9450e2946eb80bb7c3443d3e8a
amanechibana <118863897+amanechibana@users.noreply.github.com>
2024-09-24 17:27:31 -0400

    user module created

--- Structs/structs.go
+++ Structs/structs.go
@@ -2,53 +2,32 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	"example.com/structs/user"
 )
-
-type user struct {
-	firstName string
-	lastName  string
-	birthdate string
-	createdAt time.Time
-}
-
-func (u user) outputValue() {
-	fmt.Println(u.firstName, u.lastName, u.birthdate, u.createdAt)
-}
-
-func (u *user) clearUserName() {
-	u.firstName = ""
-	u.lastName = ""
-}
-
-func newUser(firstName, lastName, birthdate string) *user {
-	if firstName == "" || lastName == "" || birthdate == "" {
-
-	}
-
-	return &user{
-		firstName,
-		lastName,
-		birthdate,
-		time.Now(),
-	}
-}
 
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
 	userBirth := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	var appUser *user = newUser(userFirstName, userLastName, userBirth)
+	var appUser *user.User
 
-	appUser.outputValue()
-	appUser.clearUserName()
-	appUser.outputValue()
+	appUser, err := user.NewUser(userFirstName, userLastName, userBirth)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	appUser.OutputValue()
+	appUser.ClearUserName()
+	appUser.OutputValue()
 }
 
 func getUserData(promptText string) string {
 	fmt.Print(promptText)
 	var value string
-	fmt.Scan(&value)
+	fmt.Scanln(&value)
 	return value
 }
