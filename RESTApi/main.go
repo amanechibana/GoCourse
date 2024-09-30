58c7ca8257669594af49918f6fc6cfb35eda560e
amanechibana <118863897+amanechibana@users.noreply.github.com>
2024-09-30 14:51:09 -0400

    api update

--- RESTApi/main.go
+++ RESTApi/main.go
@@ -5,6 +5,7 @@ import (
 	"example.com/restapi/db"
 	"example.com/restapi/models"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 func main() {
@@ -12,6 +13,7 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents) // GET, POST, PUT, PATCH, DELETE
+	server.GET("/events/:id",getEvent) // /event/1 /event/5
 	server.POST("/events", createEvent)
 
 	server.Run(":8080") // localhost: 8080
@@ -23,7 +25,25 @@ func getEvents(context *gin.Context) {
 		context.JSON(http.StatusInternalServerError,gin.H{"message":"Could not fetch events."})
 		return
 	}
+	
 	context.JSON(http.StatusOK,events)
+}
+
+func getEvent(context *gin.Context){
+	eventID,err := strconv.ParseInt(context.Param("id"),10,64)
+	if err != nil{
+		context.JSON(http.StatusBadRequest,gin.H{"message":"Could not fetch event."})
+		return
+	}
+
+	event,err := models.GetEventByID(eventID)
+
+	if err != nil{
+		context.JSON(http.StatusInternalServerError,gin.H{"message":"Could not fetch event."})
+		return		
+	}
+
+	context.JSON(http.StatusOK,event)	
 }
 
 func createEvent(context *gin.Context) {
--- RESTApi/models/event.go
+++ RESTApi/models/event.go
@@ -14,7 +14,7 @@ type Event struct{
 	UserID		int
 }
 
-var events = []Event{}
+//var events = []Event{}
 
 func (e Event) Save() error {
 	query := `
@@ -61,4 +61,17 @@ func GetAllEvents() ([]Event,error) {
 		events = append(events, event)
 	}
 	return events,nil
+}
+
+func GetEventByID(id int64) (*Event,error){
+	query := "SELECT * FROM events WHERE id = ?"
+	row := db.DB.QueryRow(query, id)
+
+	var event Event
+	err:= row.Scan(&event.ID,&event.Name,&event.Description,&event.Location,&event.DateTime,&event.UserID)
+	if err!= nil{
+		return nil,err
+	}
+
+	return &event, nil
 }
