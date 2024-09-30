5a73e78e05c2d1d25c54aeac544d23eb8764551a
amanechibana <118863897+amanechibana@users.noreply.github.com>
2024-09-30 01:00:45 -0400

    rest api final project started

---
+++ RESTApi/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"net/http"
+	"example.com/restapi/db"
+	"example.com/restapi/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db.InitDB()
+	server := gin.Default()
+
+	server.GET("/events", getEvents) // GET, POST, PUT, PATCH, DELETE
+	server.POST("/events", createEvent)
+
+	server.Run(":8080") // localhost: 8080
+}
+
+func getEvents(context *gin.Context) {
+	events, err :=  models.GetAllEvents()
+	if err != nil{
+		context.JSON(http.StatusInternalServerError,gin.H{"message":"Could not fetch events."})
+		return
+	}
+	context.JSON(http.StatusOK,events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message":"Could not parse data"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	err = event.Save()
+
+	if err!= nil{
+		context.JSON(http.StatusInternalServerError,gin.H{"message":"Could not create events."})	
+		return	 
+	}
+	context.JSON(http.StatusCreated, gin.H{"message":"Event created!","event":event})
+}
